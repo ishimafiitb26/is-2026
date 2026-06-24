94ec2d64416809ba1d0ec5eac38ebbcf0933611a
zhuoyaazh <najtnsyf12@gmail.com>
2026-06-24 12:26:57 +0700

    local active

--- middleware.ts
+++ middleware.ts
@@ -4,21 +4,21 @@ import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
   // Mengecek status dari Environment Variable
   const isMaintenance = process.env.NEXT_PUBLIC_MAINTENANCE_MODE === 'true';
+  
+  // Deteksi apakah sedang di environment local (npm run dev)
+  const isDevelopment = process.env.NODE_ENV === 'development';
 
-  // Jika status maintenance aktif dan pengunjung BUKAN di halaman /maintenance
-  if (isMaintenance && !request.nextUrl.pathname.startsWith('/maintenance')) {
-    // Arahkan paksa ke halaman maintenance
+  // JIKA SEDANG MAINTENANCE & BUKAN DI LOCALHOST & BUKAN DI HALAMAN /maintenance
+  if (isMaintenance && !isDevelopment && !request.nextUrl.pathname.startsWith('/maintenance')) {
     return NextResponse.redirect(new URL('/maintenance', request.url));
   }
 
   // Jika maintenance SUDAH SELESAI, tapi user iseng buka link /maintenance
-  if (!isMaintenance && request.nextUrl.pathname.startsWith('/maintenance')) {
-    // Arahkan kembali ke halaman utama
+  if (!isMaintenance && !isDevelopment && request.nextUrl.pathname.startsWith('/maintenance')) {
     return NextResponse.redirect(new URL('/', request.url));
   }
 }
 
 export const config = {
-  // Mengecualikan file statis, gambar, dan favicon agar desain halaman maintenance tidak rusak
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
